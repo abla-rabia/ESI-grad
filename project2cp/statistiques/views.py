2b8ef4e57d64a89b084b59c0380eeb9a05ea5093
Abla <la_rabia@esi.dz>
2023-03-26 14:58:39 +0100

    hello

--- project2cp/statistiques/views.py
+++ project2cp/statistiques/views.py
@@ -3,4 +3,7 @@ from webapp.models import Doctorant
 
 # Create your views here.
 def index(request):
-    return render(request,'statistiques/index.html')
+    doc=Doctorant.objects.filter(type_doc='LMD')
+    doc2=Doctorant.objects.filter(type_doc='Classique')
+    count={'doc' : doc.count(),'doc2' : doc2.count()}
+    return render(request,'statistiques/index.html',count)
